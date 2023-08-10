e8c41e773a340214a549438396bc611394d106d3
HaEunIdevel <hekim@idevel.kr>
2023-08-10 16:14:44 +0900

    creata database & connect

--- createDB.php
+++ createDB.php
@@ -2,22 +2,25 @@
 $host = "localhost";
 $user = "root";
 $pw = "root";
-$dbConnect = new mysqli($host, $user, $pw); // mysql 접속하기
+$dbName = "phpMySQL";
+$dbConnect = new mysqli($host, $user, $pw, $dbName); // mysql 접속하기
 $dbConnect->set_charset("utf8");
 
 if (mysqli_connect_errno()) {
     echo "데이터 베이스 접속실패";
+    //
+    //} else {
+    //    $sql = "CREATE DATABASE phpMySQL";
+    //    $res = $dbConnect->query($sql); //dbConnect라는 변수에 query메소드 추가(데이터 베이스 생성)
+    //    if ($res) {
+    //        echo "데이터베이스 생성!!!";
+    //
+    //    } else {
+    //        echo "데이터베이스 생성 실패.";
+    //    }
 
 } else {
-    $sql = "CREATE DATABASE phpMySQL";
-    $res = $dbConnect->query($sql); //dbConnect라는 변수에 query메소드 추가(데이터 베이스 생성)
-    if ($res) {
-        echo "데이터베이스 생성!!!";
-
-    } else {
-        echo "데이터베이스 생성 실패.";
-    }
-
+    echo "접속 성공";
 }
 
 // 생성한 DB에 접속하기. 네번째 인자로 데이터 베이스명 입력
