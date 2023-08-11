17d367978f4cce51730046b1e3d1946fe4ccb870
JJHaEun <yj05037@gmail.com>
2023-08-11 11:49:53 +0900

    create DB & connect DB

--- createDB.php
+++ createDB.php
@@ -2,9 +2,9 @@
 $host = "localhost";
 $user = "root";
 $pw = "root";
-$dbName = "phpMySQL";
-$dbConnect = new mysqli($host, $user, $pw, $dbName); // mysql 접속하기
+$dbConnect = new mysqli($host, $user, $pw); // mysql 접속하기
 $dbConnect->set_charset("utf8");
+
 
 if (mysqli_connect_errno()) {
     echo "데이터 베이스 접속실패";
@@ -18,10 +18,27 @@ if (mysqli_connect_errno()) {
     //    } else {
     //        echo "데이터베이스 생성 실패.";
     //    }
-
+    
 } else {
     echo "접속 성공";
-}
+    
+    
+    $sql = "CREATE DATABASE phpSQL";
+    $res = $dbConnect->query($sql);
+    $dataBaseConnect = "phpSQL";
+    
+    
+    // 생성한 DB에 접속하기. 네번째 인자로 데이터 베이스명 입력
+    $dbConnect = new mysqli($host, $user, $pw,$dataBaseConnect); // mysql 접속하기
+    $dbConnect->set_charset("utf8");
+    if(mysqli_connect_errno()){
+        echo "데이터 메이스 $dataBaseConnect 에 접속 실패";
+        
+    }else{
+        echo "데이터 베이스 $dataBaseConnect 에 연결성공";
+    }}
+    
+    
+    
 
-// 생성한 DB에 접속하기. 네번째 인자로 데이터 베이스명 입력
 
